f3e617703c364db5c3c96851fe23e84cdd96bfa4
cocco_git <g33k@paranoici.org>
2017-11-18 18:04:25 +0100

    BUG found in file acquisition, gid continuity was broken with disasters, now solved, maybe output to file and the import isnt the best way, but allow for post processing, which is good

--- src/pymodule/NetGrowth/dataIO.py
+++ src/pymodule/NetGrowth/dataIO.py
@@ -114,18 +114,24 @@ def NeuronsFromSimulation(simulation_path, population_to_singles=False):
     except:
         print ("WARNING: {} not found: Neurons have already been splitted".format(os.path.join(simulation_path,"morphology.swc")))
         imported_list = [os.path.join(simulation_path, f) for f in listdir(simulation_path) if f.endswith(".swc") and f != "morphology.swc"]
-        gids = len(imported_list)
+    gids= len(imported_list)
 
     print( "This population has {} neurons".format(gids))
-    for gid, file_ in enumerate(imported_list):
-        # print("imported_file: ", imported_file)
+    def parse_gid(_file):
+        with open(file_,'r') as fp:
+            line = fp.readline()
+            line = line.split()
+            assert(line[0] == "#gid")
+            return line[1].rstrip()
+
+    for file_ in imported_list:
+        gid = parse_gid(file_)
         netgrowth_format = {"gid":gid,"data":file_}
         neurons[gid]=netgrowth_format
     try:
         info=json.load(open(os.path.join(simulation_path,"info.json")))
     except:
         raise ValueError("ERROR: {} not found".format(os.path.join(simulation_path,"info.json")))
-
     info["gids"]=gids
     neurons = {"neurons":neurons,"info":info}
     return neurons

a378ff7d2868f790cee92f419b892451538cf503
Silmathoron <Silmathoron@users.noreply.github.com>
2019-07-18 16:22:26 +0200

    updated boost + mac pip

--- extra/install_macos.sh
+++ extra/install_macos.sh
@@ -8,15 +8,15 @@ brew install gcc@8 cmake python3 geos doxygen boost
 
 pip3 install setuptools
 pip3 install cython
-pip3 install --user numpy scipy pint pyneuroml
-pip3 install --user sphinx breathe sphinx-bootstrap-theme
+pip3 install numpy scipy pint pyneuroml
+pip3 install sphinx breathe sphinx-bootstrap-theme
 pip3 install --no-binary shapely, shapely
 pip3 install matplotlib networkx nngt svg.path dxfgrabber PyOpenGL
 
 cd ..
 mkdir build
 cd build
-CC=gcc-8 CXX=g++-8 cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -Dwith-docs=ON -Dwith-python=3
+CC=gcc-8 CXX=g++-8 cmake .. -Dwith-docs=ON -Dwith-python=3
 
 make
 CC=gcc-8 CXX=g++-8 make install

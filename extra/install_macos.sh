f73f0be97a4f346fb54c7df38309f02b05ce2905
Silmathoron <Silmathoron@users.noreply.github.com>
2019-07-18 16:57:50 +0200

    upgrade to bionic, stdio error mac, test windows

--- extra/install_macos.sh
+++ extra/install_macos.sh
@@ -4,7 +4,7 @@
 
 brew tap homebrew/core
 
-brew install gcc@8 cmake python3 geos doxygen boost
+brew install gcc@8 cmake python3 geos doxygen boost libomp
 
 pip3 install setuptools
 pip3 install cython

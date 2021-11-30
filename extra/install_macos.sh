1086896ecac5c0a6b118f6adecb78502eb03d19f
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-11-30 20:33:07 +0100

    try to fix macos issue 3

--- extra/install_macos.sh
+++ extra/install_macos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap homebrew/core
@@ -8,12 +10,12 @@ brew install gcc@10 cmake python3 geos doxygen boost libomp
 
 brew link gcc@10
 
-pip3 install --user setuptools
-pip3 install --user cython
-pip3 install --user numpy scipy pint pyneuroml
-pip3 install --user sphinx breathe sphinx-bootstrap-theme
-pip3 install --user --no-binary shapely, shapely
-pip3 install --user matplotlib networkx nngt svg.path dxfgrabber PyOpenGL
+pip3 install setuptools
+pip3 install cython
+pip3 install numpy scipy pint pyneuroml
+pip3 install sphinx breathe sphinx-bootstrap-theme
+pip3 install --no-binary shapely, shapely
+pip3 install matplotlib networkx nngt svg.path dxfgrabber PyOpenGL
 
 cd ..
 mkdir build
@@ -22,4 +24,3 @@ CC=gcc-10 CXX=g++-10 cmake .. -Dwith-docs=ON -Dwith-python=3
 
 make
 CC=gcc-10 CXX=g++-10 make install
-make doc

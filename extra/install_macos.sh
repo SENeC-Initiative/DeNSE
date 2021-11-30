09e12c8c9f3de20324b8aaf298f1a725f3612ce3
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-11-30 19:39:30 +0100

    try to fix macos issue 2

--- extra/install_macos.sh
+++ extra/install_macos.sh
@@ -8,12 +8,12 @@ brew install gcc@10 cmake python3 geos doxygen boost libomp
 
 brew link gcc@10
 
-pip3 install setuptools
-pip3 install cython
-pip3 install numpy scipy pint pyneuroml
-pip3 install sphinx breathe sphinx-bootstrap-theme
-pip3 install --no-binary shapely, shapely
-pip3 install matplotlib networkx nngt svg.path dxfgrabber PyOpenGL
+pip3 install --user setuptools
+pip3 install --user cython
+pip3 install --user numpy scipy pint pyneuroml
+pip3 install --user sphinx breathe sphinx-bootstrap-theme
+pip3 install --user --no-binary shapely, shapely
+pip3 install --user matplotlib networkx nngt svg.path dxfgrabber PyOpenGL
 
 cd ..
 mkdir build

4b388400517fa743bdd060776c7c86849239b382
Tanguy Fardet <tanguy.fardet@protonmail.com>
2024-01-29 20:43:41 +0100

    Force upgrade shapely, fix conda string

--- extra/install_debian.sh
+++ extra/install_debian.sh
@@ -2,12 +2,12 @@
 
 sudo apt install cmake g++ python3 python3-dev python3-pip libgeos++-dev doxygen python3-matplotlib python3-tk libboost-dev
 
-pip3 install --user setuptools
-pip3 install --user "cython<3"
-pip3 install --user numpy scipy pint pyneuroml
-pip3 install --user --no-binary shapely, shapely
-pip3 install --user sphinx breathe sphinx-bootstrap-theme
-pip3 install --user networkx nngt svg.path dxfgrabber
+pip3 install setuptools
+pip3 install "cython<3"
+pip3 install numpy scipy pint pyneuroml
+pip3 install --upgrade --no-binary shapely, shapely
+pip3 install sphinx breathe sphinx-bootstrap-theme
+pip3 install networkx nngt svg.path dxfgrabber
 
 cd ..
 mkdir build

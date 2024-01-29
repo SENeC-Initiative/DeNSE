2aef13a07362a84ab228312a7637ce8805ffb3c8
Tanguy Fardet <tanguy.fardet@protonmail.com>
2024-01-29 19:59:31 +0100

    Remove PyOpenGL for ubuntu, use conda for windows

--- extra/install_debian.sh
+++ extra/install_debian.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-sudo apt install cmake g++ python3 python3-dev python3-pip libgeos++-dev doxygen python3-matplotlib python3-tk libboost-dev freeglut3
-
-# workaround for PyOpenGL
-for lg in /usr/lib/x86_64-linux-gnu/libglut.so.3.*; do
-    sudo ln -s "${lg}" /usr/lib/x86_64-linux-gnu/libglut.so.3
-    sudo ln -s "${lg}" /usr/lib/x86_64-linux-gnu/libglut.so
-done
+sudo apt install cmake g++ python3 python3-dev python3-pip libgeos++-dev doxygen python3-matplotlib python3-tk libboost-dev
 
 pip3 install --user setuptools
 pip3 install --user "cython<3"
 pip3 install --user numpy scipy pint pyneuroml
 pip3 install --user --no-binary shapely, shapely
 pip3 install --user sphinx breathe sphinx-bootstrap-theme
-pip3 install --user networkx nngt svg.path dxfgrabber PyOpenGL
+pip3 install --user networkx nngt svg.path dxfgrabber
 
 cd ..
 mkdir build
